5ad1065978cb028d8f278ddcd17792048cd9a085
Martin Thomson <martin.thomson@gmail.com>
2014-09-17 13:07:05 -0700

    Getting started on media section

--- media/webrtc/signaling/src/sdp/SipccSdpMediaSection.h
+++ media/webrtc/signaling/src/sdp/SipccSdpMediaSection.h
@@ -5,9 +5,24 @@
 #ifndef _SIPCCSDPMEDIASECTION_H_
 #define _SIPCCSDPMEDIASECTION_H_
 
+#include "signaling/src/sdp/sipcc/sdp.h"
+
+namespace mozilla {
+
+class SipccSdp;
+
 class SipccSdpMediaSection : public SdpMediaSection
 {
-public:
+  friend class SipccSdp;
+private:
+  SipccSdpMediaSection(sdp_t* sdp, uint16_t level)
+    : mSdp(sdp), mLevel(level) {}
+  ~SipccSdpMediaSection() {}
+
+  // this is just on loan, so we don't delete on destruct;
+  // this doesn't live beyond the SipccSdp instance that owns this
+  sdp_t* mSdp;
+  uint16_t mLevel;
 };
 
 }
