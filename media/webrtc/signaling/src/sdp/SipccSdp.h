c8df2d3f17e3433c9b8e52b1af1cf96bb274ade0
Martin Thomson <martin.thomson@gmail.com>
2014-09-17 10:09:53 -0700

    Adding sipcc specialization

---
+++ media/webrtc/signaling/src/sdp/SipccSdp.h
@@ -0,0 +1,30 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef _sdp_h_
+#define _sdp_h_
+
+#include "Attributes.h"
+#include "UniquePtr.h"
+
+#include "signaling/src/sdp/sipcc/sdp.h"
+#include "signaling/src/sdp/Sdp.h"
+
+namespace mozilla {
+
+class SipccSdpParser;
+
+class SipccSdp MOZ_FINAL : public Sdp
+{
+  friend class SipccSdpParser;
+private:
+  SipccSdp(sdp_t* aSdp) : mSdp(aSdp) {}
+  ~SipccSdp() {}
+
+  UniquePtr<sdp_t> mSdp;
+};
+
+} // namespace mozilla
+
+#endif // _sdp_h_
