b60d84f7198b1f42c1e25712212b0380ca84955e
Martin Thomson <martin.thomson@gmail.com>
2014-09-17 15:50:30 -0700

    Moving attribute lists around

--- media/webrtc/signaling/src/sdp/SdpAttribute.h
+++ media/webrtc/signaling/src/sdp/SdpAttribute.h
@@ -2,8 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef _SDP_ATTRIBUTE_H_
-#define _SDP_ATTRIBUTE_H_
+#ifndef _SDPATTRIBUTE_H_
+#define _SDPATTRIBUTE_H_
 
 #include "mozilla/UniquePtr.h"
 #include "mozilla/Attributes.h"
@@ -507,44 +507,6 @@ public:
 private:
   std::string mTypeName;
   std::string mValue;
-};
-
-class SdpAttributeList
-{
-public:
-  virtual unsigned int CountAttributes(SdpAttributeType type) const = 0;
-  virtual bool HasAttribute(SdpAttributeType type) const = 0;
-
-  virtual UniquePtr<SdpAttribute>
-    GetAttribute(sdp::AttributeType type) const = 0;
-
-  virtual UniquePtr<SdpCandidateAttribute> GetCandidate() const = 0;
-  virtual UniquePtr<SdpConnectionAttribute> GetConnection() const = 0;
-  virtual UniquePtr<SdpExtmapAttribute> GetExtmap() const = 0;
-  virtual UniquePtr<SdpFingerprintAttribute> GetFingerprint() const = 0;
-  virtual UniquePtr<SdpFmtpAttribute> GetFmtp() const = 0;
-  virtual UniquePtr<SdpGroupAttribute> GetGroup() const = 0;
-  virtual UniquePtr<SdpIceOptionsAttribute> GetIceOptions() const = 0;
-  virtual std::string GetIcePwd() const = 0;
-  virtual std::string GetIceUfrag() const = 0;
-  virtual UniquePtr<SdpIdentityAttribute> GetIdentity() const = 0;
-  virtual UniquePtr<SdpImageattrAttribute> GetImageattr() const = 0;
-  virtual std::string GetLabel() const = 0;
-  virtual unsigned int GetMaxprate() const = 0;
-  virtual unsigned int GetMaxptime() const = 0;
-  virtual std::string GetMid() const = 0;
-  virtual UniquePtr<SdpMsidAttribute> GetMsid() const = 0;
-  virtual unsigned int GetPtime() const = 0;
-  virtual UniquePtr<SdpRtcpAttribute> GetRtcp() const = 0;
-  virtual UniquePtr<SdpRtcpFbAttribute> GetRtcpFb() const = 0;
-  virtual UniquePtr<SdpRtcpRemoteCandidates> GetRemoteCandidates() const = 0;
-  virtual UniquePtr<SdpRtpmapAttribute> GetRtpmap() const = 0;
-  virtual UniquePtr<SdpSctpmapAttribute> GetSctpmap() const = 0;
-  virtual UniquePtr<SdpSetupAttribute> GetSetup() const = 0;
-  virtual UniquePtr<SdpSsrcAttribute> GetSsrc() const = 0;
-  virtual UniquePtr<SdpSsrcGroupAttribute> GetSsrcGroup() const = 0;
-
-  virtual SetAttribute(const SdpAttribute &) = 0;
 };
 
 }
