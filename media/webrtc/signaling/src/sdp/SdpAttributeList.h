509fe55c303ef4611b018bdad38e208ad49fc540
Adam Roach <adam@nostrum.com>
2014-09-17 20:31:52 -0700

    Making some attributes into lists

--- media/webrtc/signaling/src/sdp/SdpAttributeList.h
+++ media/webrtc/signaling/src/sdp/SdpAttributeList.h
@@ -24,32 +24,39 @@ public:
   virtual bool HasAttribute(AttributeType type) const = 0;
   virtual const SdpAttribute& GetAttribute(AttributeType type, size_t index = 0) const = 0;
 
-  virtual const SdpCandidateAttribute& GetCandidate() const = 0;
   virtual const SdpConnectionAttribute& GetConnection() const = 0;
-  virtual const SdpExtmapAttribute& GetExtmap() const = 0;
   virtual const SdpFingerprintAttribute& GetFingerprint() const = 0;
-  virtual const SdpFmtpAttribute& GetFmtp() const = 0;
   virtual const SdpGroupAttribute& GetGroup() const = 0;
   virtual const SdpIceOptionsAttribute& GetIceOptions() const = 0;
-  virtual const std::string& GetIcePwd() const = 0;
-  virtual const std::string& GetIceUfrag() const = 0;
   virtual const SdpIdentityAttribute& GetIdentity() const = 0;
-  virtual const SdpImageattrAttribute& GetImageattr() const = 0;
-  virtual const std::string& GetLabel() const = 0;
-  virtual uint32_t GetMaxprate() const = 0;
-  virtual uint32_t GetMaxptime() const = 0;
-  virtual const std::string& GetMid() const = 0;
   virtual const SdpMsidAttribute& GetMsid() const = 0;
-  virtual uint32_t GetPtime() const = 0;
   virtual const SdpRtcpAttribute& GetRtcp() const = 0;
-  virtual const SdpRtcpFbAttribute& GetRtcpFb() const = 0;
   virtual const SdpRemoteCandidatesAttribute& GetRemoteCandidates() const = 0;
-  virtual const SdpRtpmapAttribute& GetRtpmap() const = 0;
-  virtual const SdpSctpmapAttribute& GetSctpmap() const = 0;
   virtual const SdpSetupAttribute& GetSetup() const = 0;
   virtual const SdpSsrcAttribute& GetSsrc() const = 0;
   virtual const SdpSsrcGroupAttribute& GetSsrcGroup() const = 0;
 
+  // These attributes can appear multiple times, so the returned
+  // classes actually represent a collection of values.
+  virtual const SdpCandidateAttributeList& GetCandidate() const = 0;
+  virtual const SdpExtmapAttributeList& GetExtmap() const = 0;
+  virtual const SdpFmtpAttributeList& GetFmtp() const = 0;
+  virtual const SdpImageattrAttributeList& GetImageattr() const = 0;
+  virtual const SdpRtcpFbAttributeList& GetRtcpFb() const = 0;
+  virtual const SdpRtpmapAttributeList& GetRtpmap() const = 0;
+  virtual const SdpSctpmapAttributeList& GetSctpmap() const = 0;
+
+  // These attributes are effectively simple types, so we'll make life
+  // easy by just returning their value.
+  virtual const std::string& GetIcePwd() const = 0;
+  virtual const std::string& GetIceUfrag() const = 0;
+  virtual const std::string& GetLabel() const = 0;
+  virtual unsigned int GetMaxprate() const = 0;
+  virtual unsigned int GetMaxptime() const = 0;
+  virtual const std::string& GetMid() const = 0;
+  virtual unsigned int GetPtime() const = 0;
+
+  // The setter takes an attribute of any type, and Does The Right Thing™
   virtual void SetAttribute(const SdpAttribute &) = 0;
 };
 
