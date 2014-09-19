5aeb152797376482360024378c1a3be24d6760b3
EKR <ekr@rtfm.com>
2014-09-18 23:39:22 -0700

    Have a list of codecs that get automatically added

--- media/webrtc/signaling/src/jsep/JsepCodecDescription.h
+++ media/webrtc/signaling/src/jsep/JsepCodecDescription.h
@@ -6,13 +6,35 @@
 #define _JSEPCODECDESCRIPTION_H_
 
 #include <string>
+#include "signaling/src/sdp/SdpMediaSection.h"
 
 namespace mozilla {
 namespace jsep {
 
 // A single entry in our list of known codecs.
 struct JsepCodecDescription {
-  
+  JsepCodecDescription(mozilla::SdpMediaSection::MediaType type,
+                       uint8_t default_pt,
+                       const std::string& name,
+                       uint32_t clock,
+                       uint32_t channels,
+                       bool enabled = true) :
+      mType(type),
+      mDefaultPt(default_pt),
+      mName(name),
+      mClock(clock),
+      mChannels(channels),
+      mEnabled(enabled) {}
+
+  // These should be const except for enabled, but then
+  // I can't assign, which is sad.
+
+  mozilla::SdpMediaSection::MediaType mType;
+  uint8_t mDefaultPt;
+  std::string mName;
+  uint32_t mClock;
+  uint32_t mChannels;
+  bool mEnabled;
 };
 
 }  // namespace jsep
