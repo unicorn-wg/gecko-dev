7742cdc6b6a746f503f13b2af925d16f3e8ce126
EKR <ekr@rtfm.com>
2014-09-18 23:06:51 -0700

    Added

---
+++ media/webrtc/signaling/src/jsep/JsepCodecDescription.h
@@ -0,0 +1,21 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef _JSEPCODECDESCRIPTION_H_
+#define _JSEPCODECDESCRIPTION_H_
+
+#include <string>
+
+namespace mozilla {
+namespace jsep {
+
+// A single entry in our list of known codecs.
+struct JsepCodecDescription {
+  
+};
+
+}  // namespace jsep
+}  // namespace mozilla
+
+#endif
