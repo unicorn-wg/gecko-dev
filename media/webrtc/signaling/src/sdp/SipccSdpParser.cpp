8aa918c4b66032c92674aed0c4e22408c49ec06e
Byron Campen <docfaraday@gmail.com>
2014-10-23 10:28:08 -0700

    Add a missing include.

--- media/webrtc/signaling/src/sdp/SipccSdpParser.cpp
+++ media/webrtc/signaling/src/sdp/SipccSdpParser.cpp
@@ -5,6 +5,7 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "signaling/src/sdp/SipccSdpParser.h"
+#include "signaling/src/sdp/SipccSdp.h"
 
 #include <utility>
 extern "C" {
